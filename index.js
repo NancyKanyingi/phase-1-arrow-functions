c11b6bf392fe571ded860063fc0a4f7e9d6b55e1
Nancy <wangui.kanyingi2015@gmail.com>
2025-07-05 16:09:15 +0300

    Adds index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+function divide (a,b){
+    // a = 200
+    // b = 100
+    return a/b
+}
+let result = divide(2000, 100)
+console.log(result)
+
+let nums = [2000, 100]
+// let square =nums.map (x => x * x) {}
+
+const square = (x) => {
+    return x * x;
+}
+console.log (square (2))
+const add = (a,b)=>{
+    return a +b
+}
+console.log(add(3,4))
+
+
